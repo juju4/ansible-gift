ecf86f1c0c94b1f23432f9e71921e07fd3e8b319
juju4 <juju4@users.noreply.github.com>
2022-09-03 13:50:08 +0000

    remove older distribution references like trusty

--- test/integration/default/serverspec/giftppa_spec.rb
+++ test/integration/default/serverspec/giftppa_spec.rb
@@ -9,12 +9,7 @@ set :backend, :exec
 #    c.formatter = 'JUnit'
 #end
 
-describe file('/etc/apt/sources.list.d/ppa_gift_stable_xenial.list'), :if => os[:family] == 'ubuntu' && os[:release] == '16.04' do
+describe file('/etc/apt/sources.list.d/ppa_gift_stable_*.list'), :if => os[:family] == 'ubuntu' do
   it { should exist }
-  its(:content) { should match /deb http:\/\/ppa.launchpad.net\/gift\/stable\/ubuntu xenial main/ }
-end
-
-describe file('/etc/apt/sources.list.d/ppa_gift_stable_trusty.list'), :if => os[:family] == 'ubuntu' && os[:release] == '14.04' do
-  it { should exist }
-  its(:content) { should match /deb http:\/\/ppa.launchpad.net\/gift\/stable\/ubuntu trusty main/ }
+  its(:content) { should match /deb http:\/\/ppa.launchpad.net\/gift\/stable\/ubuntu / }
 end
